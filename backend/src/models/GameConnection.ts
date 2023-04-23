ac0a03c354d83f74674645961cd9a7c5d0041bb6
Alex Griffiths <axgriffiths67@gmail.com>
2023-04-23 13:50:01 -0500

    Game chat

--- backend/src/models/GameConnection.ts
+++ backend/src/models/GameConnection.ts
@@ -1,7 +1,9 @@
 import { Chess } from "chess.js";
+import { ChatMessage } from "./ChatMessage";
 import { Player } from "./Player";
 export type GameConnection = {
     game: Chess, 
     white: Player,
     black?: Player
+    chat?: ChatMessage[]
 }
